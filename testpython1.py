5f702ee5abac70b6af3a85f95135adb4bef6ff31
Amos Froese van Dijck <amosfroese@gmail.com>
2023-08-04 13:35:39 +0200

    ;

--- testpython1.py
+++ testpython1.py
@@ -1,6 +1,9 @@
 
 import pyscript
 from pyscript import Element
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 dataset = {'0':[0,1,2,3,2,1,0], '1':[5,4,3,2,1,0]}
 
@@ -13,3 +16,9 @@ def get_data(*args, **kwargs):
 
     data = dataset[data_key]
     result_place.element.innerText = data
+    make_table(data)
+
+def make_table(data):
+    df = pd.DataFrame(data)
+    plt.hist(df[0])
+    plt.show()

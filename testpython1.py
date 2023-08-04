ae798e1a978609cecfb3ed5d97f90da257a777a8
Amos Froese van Dijck <amosfroese@gmail.com>
2023-08-04 11:07:05 +0200

    a:
    ?

--- testpython1.py
+++ testpython1.py
@@ -5,6 +5,7 @@ def get_data(*args, **kwargs):
     result_place = Element['result']
     #result_place.write(f"{Element('name').value} is a good human from {Element('countries').value}")
     data = dataset[Element("unique_identifyer").value]
+    result_place.write(data)
     print(data)
 
 #dataset = {"00000":[0,1,2,3,2,1,0], "00001":[5,4,3,2,1,0]}

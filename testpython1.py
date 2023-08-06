7b803f57b697842b809570a56aa90ed621c2b9a7
Amos Froese van Dijck <amosfroese@gmail.com>
2023-08-06 12:13:33 +0200

    ;

--- testpython1.py
+++ testpython1.py
@@ -10,16 +10,26 @@ dataset = {'0':[0,1,2,3,2,1,0], '1':[5,4,3,2,1,0]}
 def greetings(name):
     print(f'Hi, {name}')
 
-def get_data(*args, **kwargs):
-    result_place = Element('result')
-    data_key = Element("unique_identifyer").value
+def get_data(data_key):
+    if data_key in dataset:
+        data_frame = get_data_df(dataset[data_key])
+        message = "loading Data for entered Unique Identifyer"
+    else:
+        message = "Invalid Unique Identifyer"
+    return message, Data_frame
 
-    data = dataset[data_key]
-    result_place.element.innerText = data
-    return make_table(data)
-
-def make_table(data):
+def get_data_df(data):
     df = pd.DataFrame(data)
-    plt.hist(df[0])
-    
-    return plt
+    return def
+
+    #takes in a pandas dataframe "data" and a plot type
+def get_data_plot(data):
+    fig, ax = plt.subplots()
+    plt.bar(data.index, data[0])
+    # Naming the x-label
+    plt.xlabel('Visit')
+    # Naming the y-label
+    plt.ylabel('Poem Score')
+    # Naming the title of the plot
+    plt.title('Patient Data - POEM score vs. Visit')
+    return fig
